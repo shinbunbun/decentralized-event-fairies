d8e221ae142a7bc32425686ec415f23c688f9af0
shinbunbun <takamuneyuto@gmail.com>
2022-10-29 16:25:46 +0900

    issuerを修正

--- dids/src/issuer.rs
+++ dids/src/issuer.rs
@@ -16,10 +16,12 @@ use crate::error::Error;
 
 pub async fn issue_vc(
     subject_value: Value,
-    credential_id: &str,
+    // credential_id: &str,
     credential_type: &str,
 ) -> Result<Credential, Error> {
-    let stronghold = create_stronghold().await.map_err(Error::AccountStorage)?;
+    let stronghold = create_stronghold("./hodl/issuer.hodl")
+        .await
+        .map_err(Error::AccountStorage)?;
 
     let mut issuer: Account = Account::builder()
         .storage(stronghold)
@@ -39,7 +41,7 @@ pub async fn issue_vc(
     let subject: Subject = Subject::from_json_value(subject_value).map_err(Error::Core)?;
 
     let mut credential: Credential = CredentialBuilder::default()
-        .id(Url::parse(credential_id).map_err(Error::Core)?)
+        // .id(Url::parse(credential_id).map_err(Error::Core)?)
         .issuer(Url::parse(issuer.did().as_str()).map_err(Error::Core)?)
         .type_(credential_type)
         .subject(subject)
@@ -62,14 +64,20 @@ pub async fn issue_vc(
     Ok(credential)
 }
 
-async fn create_stronghold() -> Result<Stronghold, identity_iota::account_storage::Error> {
-    let stronghold_path: PathBuf = "./example-strong.hodl".into();
+async fn create_stronghold(
+    path: &str,
+) -> Result<Stronghold, identity_iota::account_storage::Error> {
+    let stronghold_path: PathBuf = path.into();
     let password: String = "my-password".to_owned();
     Stronghold::new(&stronghold_path, password, None).await
 }
 
-async fn create_holder_account() -> Result<Account, Error> {
+async fn create_holder_account(user_id: &str) -> Result<Account, Error> {
+    let stronghold = create_stronghold(&("./hodl/".to_string() + user_id + ".hodl"))
+        .await
+        .map_err(Error::AccountStorage)?;
     Account::builder()
+        .storage(stronghold)
         .create_identity(IdentitySetup::default())
         .await
         .map_err(Error::Account)
@@ -77,28 +85,32 @@ async fn create_holder_account() -> Result<Account, Error> {
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
     use identity_iota::core::json;
 
     #[tokio::test]
     async fn test_issue_vc() {
-        let holder = create_holder_account().await.unwrap();
+        let user_id = "39c129a2-fee8-46a9-b8e6-9635f04b66f9";
+
+        let holder = create_holder_account(user_id).await.unwrap();
 
         let subject_value = json!({
           "id": holder.document().id(),
-          "name": "Alice",
-          "degree": {
-            "type": "BachelorDegree",
-            "name": "Bachelor of Science and Arts",
-          },
-          "GPA": "4.0",
+          "user_id": user_id,
+          "eventName": "学祭ハッカソン",
+          "startDate": "2021-10-28T10:00:00+09:00",
+          "endDate": "2021-10-30T19:00:00+09:00",
+          "location": "会津大学",
+          "description": "学祭ハッカソンです。",
+          "organizer": "Zli",
         });
 
-        let credential_id = "https://example.edu/credentials/3732";
+        // let credential_id = "https://example.edu/credentials/3732";
 
-        let credential_type = "UniversityDegreeCredential";
+        let credential_type = "EventJoinProofCredential";
 
-        let credential = issue_vc(subject_value, credential_id, credential_type)
+        let credential = issue_vc(subject_value, /* credential_id, */ credential_type)
             .await
             .unwrap();
 

cd11d08b030d70796383096167b2f8df6a166a2f
Endered <yy56ga10ve@gmail.com>
2022-10-29 17:58:56 +0900

    move hasura directory to appropriate position

---
+++ apps/hasura/initialize/01-database.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS public.users(
+       id SERIAL PRIMARY KEY,
+       name TEXT NOT NULL,
+       email TEXT NOT NULL,
+       image TEXT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.events(
+       id SERIAL PRIMARY KEY,
+       title TEXT NOT NULL,
+       start_time DATE NOT NULL,
+       end_time DATE NOT NULL,
+       description TEXT NULL,
+       thumbnail TEXT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.user_admin_event(
+       id SERIAL PRIMARY KEY,
+       user_id  INTEGER REFERENCES public.users(id) NOT NULL,
+       event_id INTEGER REFERENCES public.events(id) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.user_participant_event(
+       id SERIAL PRIMARY KEY,
+       user_id INTEGER REFERENCES public.users(id) NOT NULL,
+       event_id INTEGER REFERENCES public.events(id) NOT NULL
+);

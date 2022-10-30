af94c4d14ef236fc57323c14af82bab40ab24e05
Endered <yy56ga10ve@gmail.com>
2022-10-30 15:16:12 +0900

    implement apis

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HasuraController } from './hasura.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, HasuraController],
   providers: [AppService],
 })
 export class AppModule {}

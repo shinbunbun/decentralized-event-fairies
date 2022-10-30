67a53039d6628733fb9661242738ff3761497a5f
shinbunbun <takamuneyuto@gmail.com>
2022-10-30 14:53:54 +0900

    unwrapを削除

--- dids/src/handler/verify.rs
+++ dids/src/handler/verify.rs
@@ -1,7 +1,7 @@
 use actix_web::{web, HttpResponse, Responder};
 use serde::Deserialize;
 
-use crate::verifier;
+use crate::{error::Error, verifier};
 
 #[derive(Deserialize)]
 pub struct VerifyRequest {
@@ -9,13 +9,11 @@ pub struct VerifyRequest {
     pub challenge: String,
 }
 
-pub async fn verify(body: web::Json<VerifyRequest>) -> impl Responder {
+pub async fn verify(body: web::Json<VerifyRequest>) -> Result<HttpResponse, Error> {
     let presentation_json = &body.presentation_json;
     let challenge = &body.challenge;
 
-    verifier::verify(presentation_json, challenge)
-        .await
-        .unwrap();
+    verifier::verify(presentation_json, challenge).await?;
 
-    HttpResponse::Ok().body("")
+    Ok(HttpResponse::Ok().body(""))
 }

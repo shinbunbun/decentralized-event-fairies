6bd3918dabdf155d38a59a392443bd0d0a416a09
shinbunbun <takamuneyuto@gmail.com>
2022-11-01 16:39:04 +0900

    tsに変更

--- wallet/src/background.ts
+++ wallet/src/background.ts
@@ -7,7 +7,7 @@
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'GREETINGS') {
-    const message = `Hi ${
+    const message: string = `Hi ${
       sender.tab ? 'Con' : 'Pop'
     }, my name is Bac. I am from Background. It's great to hear from you.`;
 

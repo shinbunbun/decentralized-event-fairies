af29062ef52380c7b910d407714460d1b2f23729
shinbunbun <takamuneyuto@gmail.com>
2022-11-02 12:08:05 +0900

    init identity

--- apps/frontend/src/pages/DIDTestPage.tsx
+++ apps/frontend/src/pages/DIDTestPage.tsx
@@ -1,5 +1,29 @@
+import * as identity from "@iota/identity-wasm/web";
+
 
 function DIDTestPage() {
+  (async () => {
+  
+    await identity.init()
+      
+    // The creation step generates a keypair, builds an identity
+    // and publishes it to the IOTA mainnet.
+    let builder = new identity.AccountBuilder();
+    let account = await builder.createIdentity();
+  
+    // Retrieve the DID of the newly created identity.
+    const did = account.did();
+  
+    // Print the DID of the created Identity.
+    console.log(did.toString())
+  
+    // Print the local state of the DID Document
+    console.log(account.document());
+
+    await identity.init("./static/identity_wasm_bg.wasm");
+    
+  })()
+
   return <div>
     <h1>Hello World</h1>
   </div>;

e486d4c21847a934f464181b8f993b968a31bbdd
shinbunbun <takamuneyuto@gmail.com>
2022-11-13 18:46:42 +0900

    jwtまわりの処理を実装

--- apps/backend/src/jwt/jwt.ts
+++ apps/backend/src/jwt/jwt.ts
@@ -1,7 +1,11 @@
 import * as didJWT from 'did-jwt';
-import { generateKeyPairFromSeed } from '@stablelib/ed25519'
+import { generateKeyPairFromSeed, verify } from '@stablelib/ed25519'
+import * as textEncoding from 'text-encoding';
+import * as jose from 'jose';
+import * as identity from "@iota/identity-wasm/node";
+import * as multibase from 'multibase';
 
-export interface JwtPayload {
+interface JwtPayload {
   aud: string;
   exp: number;
   iat: number;
@@ -19,4 +23,34 @@ export const createJwt = async (hexPrivateKey: string, payload: JwtPayload) => {
   )
 
   return jwt;
+}
+
+export const verifyJwt = async (jwt: string, issuer: string) => {
+  const { payload, header, signature, data } = didJWT.decodeJWT(jwt);
+  const { iss } = payload;
+  console.log(payload);
+
+  const resolver = new identity.Resolver()
+  const didDoc = await resolver.resolve(iss);
+  const { publicKeyMultibase } = didDoc.toJSON().doc.capabilityInvocation[0];
+  const publicKey = Uint8Array.from(Buffer.from(multibase.decode(publicKeyMultibase)));
+
+  const arraySignature = Uint8Array.from(Buffer.from(signature.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, ''), "base64"));
+
+  const verified = verify(publicKey, Uint8Array.from(Buffer.from(data)), arraySignature);
+  if (!verified) {
+    throw new Error("Signature is not valid");
+  }
+
+  if (iss !== issuer) {
+    throw new Error("Issuer is not valid");
+  }
+
+  const date = new Date();
+  const time = Math.floor(date.getTime() / 1000);
+  if (payload.exp < time) {
+    throw new Error("Token is expired");
+  }
+
+  return true;
 }
